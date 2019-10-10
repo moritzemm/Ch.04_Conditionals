d5383a183c6050a6a9c2d7a49bab886405767f80
moritzemm <moritzemm@urbandalkeschools.com>
2019-10-10 14:13:36 -0500

    10/10

---
+++ testing.py
@@ -0,0 +1,18 @@
+#conditionals
+'''
+a=6
+b=5
+if a < b:
+    print("a is less than b")
+elif a > b :
+    print("a is greater than b")
+else:
+    print("a is equal to b")
+'''
+
+game_over = False
+
+if game_over:
+    print("The game is over! You lose")
+else:
+    print("The game is still running!")
